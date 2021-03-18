b6430e62e8e3702282c02628f59100b4d65948cd
Tony ElHabr <anthonyelhabr@gmail.com>
2021-03-18 13:02:45 -0500

    re-test action

--- R/tweets-generate.R
+++ R/tweets-generate.R
@@ -227,8 +227,8 @@ generate_tweet <-
     }
     # browser()
     rtweet::post_tweet(
-      status = text # ,
-      # in_reply_to_status_id = pred$status_id,
+      status = text,
+      in_reply_to_status_id = pred$status_id,
       # media = c(path_png_preds, path_png_shap)
       # media = path_png_shap
     )
